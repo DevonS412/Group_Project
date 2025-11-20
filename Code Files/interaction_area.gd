0ad0c169a1fa473733900cb5aa8501e94e699fbd
Darrell2000 <ddavis811@ivytech.edu>
2025-11-20 17:57:27 -0500

    Implement interaction area for player and NPC

--- Code Files/interaction_area.gd
+++ Code Files/interaction_area.gd
@@ -1,1 +1,16 @@
+extends Area2D
+
+@onready var npc = get_parent()
+
+func _on_body_entered(body: Node2D) -> void:
+	if body.is_in_group("player"):
+		set_process_input(true)
+
+func _on_body_exited(body: Node2D) -> void:
+	if body.is_in_group("player"):
+		set_process_input(false)
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("interact"):
+		npc.start_conversation()
 
