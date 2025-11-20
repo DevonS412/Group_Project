cd4a25e5b3aa167e5de97380994ebc0c78339961
Darrell2000 <ddavis811@ivytech.edu>
2025-11-20 17:52:28 -0500

    Add camera movement logic in camera_2d.gd
    Implement camera movement based on player position.

--- Code Files/camera_2d.gd
+++ Code Files/camera_2d.gd
@@ -1,1 +1,32 @@
+extends Camera2D
 
+@export var player_path: NodePath
+var player: Node2D
+
+# distance the camera moves when player crosses edge of screen
+var move_distance: Vector2 = Vector2.ZERO
+
+func _ready() -> void:
+	player = get_node(player_path)
+	set_as_top_level(true)
+	# 1152x648 example – change to match your viewport size
+	move_distance = Vector2(1152, 648) / zoom
+
+func _physics_process(delta: float) -> void:
+	var player_pos: Vector2 = player.global_position
+	var cam_pos: Vector2 = global_position
+	var new_cam_pos: Vector2 = cam_pos
+
+	# horizontal edges
+	if player_pos.x < cam_pos.x - move_distance.x / 2.0:
+		new_cam_pos.x -= move_distance.x
+	elif player_pos.x > cam_pos.x + move_distance.x / 2.0:
+		new_cam_pos.x += move_distance.x
+
+	# vertical edges
+	if player_pos.y < cam_pos.y - move_distance.y / 2.0:
+		new_cam_pos.y -= move_distance.y
+	elif player_pos.y > cam_pos.y + move_distance.y / 2.0:
+		new_cam_pos.y += move_distance.y
+
+	global_position = new_cam_pos
